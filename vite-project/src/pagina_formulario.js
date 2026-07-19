37b8ac939bd757670372012ad28d7b64e004b281
vtcar <vtcar@gmail.com>
2026-07-19 19:21:43 -0300

    Otimizei o checkboxes do html

--- vite-project/src/pagina_formulario.js
+++ vite-project/src/pagina_formulario.js
@@ -1,3 +1,30 @@
-import './css/style.css'
 
-// Lógica de validação do formulário será adicionada aqui
+
+const opecoesAvaliacao = [
+    { value: 'tratamento', label: 'Tratamento'},
+    { value: 'localidade', label: 'Localidade'},
+    { value: 'tratamento_animais', label:'Tratamento aos animais'},
+    { value: 'condicao_predio', label:'Condição do predio'},
+    { valeu: 'informacoes' , label:'Informações'}
+]
+
+function gerarCheckboxes(opcoes,name){
+    let html = '';
+
+    opcoes.forEach(opcao => {
+        html += `
+            <div>
+                <label for="${name}_${opcao.value}">${opcao.label}</label>
+                <input type="" name="${name}" value="${opcao.value}" id ="${name}_${opcao.value}">
+            </div>
+        `;
+    });
+
+    return html;
+}
+
+const containerMais = document.getElementById('container-mais-gostou');
+const containerMenos = document.getElementById('cointer-menos-gostou');
+
+containerMais.innerHTML = gerarCheckeboxes(opecoesAvalaioacao, 'mais_gostou');
+containerMenos.innerHTML = gerarCheckboxes(opcoesAvaliacao, 'menos_gostou');
