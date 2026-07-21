32d2632d4c902969add0959d6a23a827d86e6556
TiagoSenaD <50551963+TiagoSenaD@users.noreply.github.com>
2026-07-21 14:29:14 -0300

    Tentei implementar o fetch para pegar o submit do formulário

--- vite-project/src/submit_formulario.js
+++ vite-project/src/submit_formulario.js
@@ -1,2 +1,30 @@
-let Form = document.getElementById("form_adocao_pet");
+let formElem = document.getElementById("form_adocao_pet");
 
+function formHandler(){
+    let response = await fetch('',{
+        method: 'POST',
+        body: 
+    });
+
+    if(response.ok){
+        let result = await response.formData();
+    }else{
+        alert("HTTP-Error: " + response.status);
+    }
+}
+
+formElem.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let response = await fetch('/artivle/formdata/post/user',{
+        method: 'POST',
+        body: FormData(formElem)
+    });
+
+    if (response.ok){
+        let result = await response.FormData();
+    }
+    else{
+        alert("HTTP-Error: " + response.status);
+    }
+}
